7534b0dd767b6d63a27421fde6e426a6de977859
mobin-thz <mobintaherzadeh@gmail.com>
2023-04-29 02:42:10 +0430

    The Basics

--- projects/views.py
+++ projects/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+projectslist = [
+            {
+            'id':'1',
+            'title': 'Ecommerce website',
+            'description':'Fully functional ecommerce website'
+            },
+            {
+            'id':'2',
+            'title': 'Portfolio Website',
+            'description':'This was a project where i built out my portfolio'
+            },
+            {
+            'id':'3',
+            'title': 'Social Network',
+            'description':'Awesome open source project i am still working on'
+            },
+]
 
 
 def projects (request):
+
     page=' projects '
     number= 54
-    context= {'page':page, 'number': number }
+    context= {'page':page, 'number': number, 'projects':projectslist }
+
     return render(request,'projects/projects.html', context)
 
 def project (request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectslist:
+        if i['id'] == pk: 
+            projectObj = i
+    return render(request,'projects/single-project.html', {'project': projectObj})
 
 
 # Create your views here.
