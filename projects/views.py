43bc73b88657f54fa25eb78ebc9491d354f44443
mobin-thz <mobintaherzadeh@gmail.com>
2023-04-29 02:38:19 +0430

    Rendering data to template

---
+++ projects/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+def projects (request):
+    page=' projects '
+    number= 54
+    context= {'page':page, 'number': number }
+    return render(request,'projects/projects.html', context)
+
+def project (request, pk):
+    return render(request, 'projects/single-project.html')
+
+
+# Create your views here.
