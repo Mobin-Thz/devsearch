9502bb8247244a390d183095bbb2bd6c98fe650a
mobin-thz <mobintaherzadeh@gmail.com>
2023-05-07 16:44:55 +0430

    Database Relationships

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import project
+from .models import project , review , tag
 
 admin.site.register(project)
+admin.site.register(review)
+admin.site.register(tag)
--- projects/models.py
+++ projects/models.py
@@ -6,8 +6,43 @@ class project(models.Model):
     description = models.TextField(null= True , blank= True)
     demo_link = models.CharField(max_length= 2000, null= True , blank= True)
     source_link = models.CharField(max_length= 2000, null= True , blank= True)
+    tag = models.ManyToManyField('tag', blank=True)
+    vote_total = models.IntegerField( default= 0 , blank= True, null= True)
+    vote_ratio = models.IntegerField( default= 0 ,blank= True, null= True )
     created = models.DateField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key= True, editable= False)
 
     def __str__(self):
         return self.title
+
+class review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    #owner
+    project = models.ForeignKey(project, on_delete=models.CASCADE)
+    body =  models.TextField(null= True , blank= True)
+    value = models.CharField(max_length= 200, choices = VOTE_TYPE)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key= True, editable= False)
+
+
+
+    def __str__(self):
+        return self.value
+    
+
+class tag(models.Model):
+    name = models.CharField(max_length= 200 )
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key= True, editable= False)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
