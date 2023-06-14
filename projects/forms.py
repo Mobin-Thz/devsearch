50d344c2f31425214beb1351cbd67bfab3f8fb58
mobin-thz <mobintaherzadeh@gmail.com>
2023-06-14 13:16:45 +0330

    Theme instalation

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,9 @@
-from django.forms import ModelForm
+from typing import Any, Dict, Mapping, Optional, Type, Union
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms import ModelForm , widgets
+from django import forms
+from django.forms.utils import ErrorList
 from .models import project
 
 class ProjectForm(ModelForm):
@@ -6,3 +11,15 @@ class ProjectForm(ModelForm):
         model = project 
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tag' ]
 
+        widgets = {
+            'tag' : forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args , **kwargs): 
+        super(ProjectForm, self).__init__(*args , **kwargs)
+
+        for name , field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        # self.fields['title'].widget.attrs.update(
+        #     {'class': 'input', 'placeholder': 'Add Title' })
