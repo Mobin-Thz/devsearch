c9252aae3e5715e4d1630d984b4ce184795d0dfe
mobin-thz <mobintaherzadeh@gmail.com>
2023-07-15 02:49:23 +0330

    User Registration

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
 
     
     path('', views.profiles, name='profiles'),
