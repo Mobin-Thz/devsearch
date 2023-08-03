c25821769a4d5f337e1b9dac53cc4ee1b45649c5
mobin-thz <mobintaherzadeh@gmail.com>
2023-08-03 23:23:13 +0330

    django rest framework

---
+++ api/views.py
@@ -0,0 +1,39 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import Projectserializer
+from projects.models import project
+
+
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {"GET":'/api/projects'},
+        {"GET":'/api/projects/id'},
+        {"POST":'/api/projects/id/vote'},
+
+        {"POST":'/api/users/token'},
+        {"POST":'/api/users/token/refresh'},
+    ]
+
+    return Response( routes)
+
+
+@api_view(['GET'])
+def getprojects(request):
+
+    projects = project.objects.all()
+    serializer = Projectserializer(projects, many=True)
+
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def getproject(request, pk):
+
+    Project = project.objects.get(id=pk)
+    serializer = Projectserializer(Project, many=False)
+
+    return Response(serializer.data)
